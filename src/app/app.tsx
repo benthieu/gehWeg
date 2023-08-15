241df723b0872815fa42b13dd6d132d8012fb546
CLI236\raemy <raemy@glue.ch>
2023-08-15 22:39:04 +0200

    test for supabase access

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import User from './components/Users';
 
 export function App() {
   return (
     <div>
+      <User/>
       <MapContainer center={[46.947707374681514, 7.445807175401288]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
