c408fa84a62a62e2a0d77a1d25f56277ea117af7
CLI236\raemy <raemy@glue.ch>
2023-08-24 10:32:55 +0200

    removed more unused code

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-
 import User from './components/Users';
 import FileSaver from './components/FileSaver';
 import {
   Container,
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
 
 export function App() {
-  const [email, setEmail] = useState('');
-  const user = useUser();
-  const supabase = useSupabaseClient();
-  console.log(email);
 
   return (
     <Container align="center" className="container-sm mt-4">
