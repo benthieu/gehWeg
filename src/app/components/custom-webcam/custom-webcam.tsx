77b350708343f8d1dc4422cb3ad333b28373426d
CLI236\raemy <raemy@glue.ch>
2023-08-31 16:50:38 +0200

    image format added

--- src/app/components/custom-webcam/custom-webcam.tsx
+++ src/app/components/custom-webcam/custom-webcam.tsx
@@ -1,8 +1,13 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useCallback, useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import Webcam from 'react-webcam';
+import { v4 as uuidv4 } from 'uuid';
+import { decode } from 'base64-arraybuffer';
 
-const CustomWebcam = ({turnOff}: () => void) => {
+const CustomWebcam = ({ turnOff }: () => void) => {
+  const [images, setImages] = useState([]);
+  const supabase = useSupabaseClient();
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
 
@@ -16,15 +21,53 @@ const CustomWebcam = ({turnOff}: () => void) => {
   };
 
   const savePhoto = async () => {
-    throw new Error('Not implemented');
+    console.log('imgSrc: ', imgSrc);
+
+    const { error } = await supabase.storage
+      .from('images')
+      .upload('admin/' + uuidv4(), imgSrc, {
+        contentType: 'image/png',
+      });
+
+    if (error) {
+      console.log('error: ' + typeof error, error);
+      alert('error: ' + error.error + ', ' + error.message);
+    } else {
+      alert('Image saved');
+    }
   };
+
+  async function getImages() {
+    console.log('getImges called...');
+    const { data, error } = await supabase.storage
+      .from('images')
+      .list('admin/', {
+        limit: 20,
+        offset: 0,
+        sortBy: { column: 'name', order: 'asc' },
+      });
+
+    if (data !== null) {
+      console.log('data', data);
+      setImages(data);
+    } else {
+      alert('Error loading imgaes');
+      console.log(error);
+    }
+  }
+
 
   return (
     <>
       {imgSrc ? (
         <img src={imgSrc} alt="webcam" />
       ) : (
-        <Webcam height={600} width={600} ref={webcamRef} />
+        <Webcam
+          height={600}
+          width={600}
+          ref={webcamRef}
+          screenshotFormat="image/png"
+        />
       )}
       <section>
         {imgSrc ? (
