56a40b119fa91ad4249f93e8d941ee9f8220f0fa
CLI236\raemy <raemy@glue.ch>
2023-09-07 13:19:02 +0200

    add offer button added

--- src/app/overview-map/overview-map.tsx
+++ src/app/overview-map/overview-map.tsx
@@ -1,18 +1,36 @@
 import { LatLngLiteral } from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useNavigate } from 'react-router-dom';
+import { AddOfferForm } from '../add-offer-form/add-offer-form';
 
 export function OverviewMap() {
+  const navigate = useNavigate();
+
   const locationBern: LatLngLiteral = {
     lat: 46.947707374681514,
-    lng: 7.445807175401288
+    lng: 7.445807175401288,
   };
   return (
-    <MapContainer
-      center={locationBern}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <MapContainer center={locationBern} zoom={13} scrollWheelZoom={false}>
+      <header className="map">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </header>
+      <aside>
+        <section>
+          <Fab
+            onClick={() => navigate('/offer-form')}
+            className="add-offer-button"
+            aria-label="add"
+          >
+            <AddIcon />
+          </Fab>
+          </section>
+          <section>
+            <h1>Add offer</h1>
+          </section>
+      </aside>
     </MapContainer>
   );
 }
