20f0c4180e822d86c87a0958d99db949249563a1
vlado <klepv1@bfh.ch>
2023-09-26 19:14:16 +0200

    refactoring category selection type

--- src/app/offer-list-filter/filter-category.tsx
+++ src/app/offer-list-filter/filter-category.tsx
@@ -1,4 +1,4 @@
-import {Box, MenuItem, Stack, TextField} from '@mui/material';
+import {Box, InputLabel, MenuItem, Select, SelectChangeEvent, Stack} from '@mui/material';
 import {useState} from 'react';
 
 type OfferCategoryProps = {
@@ -10,26 +10,28 @@ export function FilterCategory({categories, updateCategory}: OfferCategoryProps)
 
   const [value, setValue] = useState('');
 
+  const handleChange = (event: SelectChangeEvent) => {
+    setValue(event.target.value as string);
+    updateCategory(event.target.value as string);
+  };
+
   return (
     <Box mx={1}>
       <Stack direction="column" m={1}>
-        <TextField
-          id="filter-category"
-          label='Kategorie wählen'
-          select
-          variant="standard"
+        <InputLabel id="filter-category-label">Kategorie wählen</InputLabel>
+        <Select
+          labelId="filter-category-label"
+          id="filter-category-select"
           value={value}
-          onChange={(event) => {
-            setValue(event.target.value);
-            updateCategory(event.target.value);
-          }}
+          label="Kategorie wählen"
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <MenuItem key={category} value={category}>
               {category}
             </MenuItem>
           ))}
-        </TextField>
+        </Select>
       </Stack>
     </Box>
   );
