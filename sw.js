21afb8eef29c88d4194d57c41087ce00c50c722a
CLI236\raemy <raemy@glue.ch>
2023-10-03 11:23:53 +0200

    sw cache fetch

--- sw.js
+++ sw.js
@@ -13,3 +13,19 @@ async function cacheAssets(){
     let cache = await caches.open('gehweg');
     return cache.addAll(['/']);
 }
+
+async function fetchResource(request) {
+    let cache = await caches.open('gehweg');
+    let response = await caches.match(request);
+
+   
+    if (!response) {
+        try {
+        response = await fetch(request);
+        await cache.put(request, response.clone());
+        } catch (error) {
+            return new Response(`Resource ${request.url} not found`);
+        }
+    }
+    return response;
+} 
